05851f7d24b1fc3631107c1edeedc2869c7bd889
Hanjjong <tim4974@naver.com>
2024-07-20 23:08:29 +0900

    [feat] : feat member's id field

--- src/main/java/like_lion/phytontalk/member/Member.java
+++ src/main/java/like_lion/phytontalk/member/Member.java
@@ -1,7 +1,13 @@
 package like_lion.phytontalk.member;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 }
