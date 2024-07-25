0d80b89118e041dafbba52a382952183e74d04b4
Hanjjong <tim4974@naver.com>
2024-07-25 21:13:44 +0900

    [feat] : feat mailsend(), generateRandomCode()

--- src/main/java/like_lion/phytontalk/email/MailServiceImpl.java
+++ src/main/java/like_lion/phytontalk/email/MailServiceImpl.java
@@ -1,11 +1,34 @@
 package like_lion.phytontalk.email;
 
 import like_lion.phytontalk.email.dto.MailAuthenticateResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
+@RequiredArgsConstructor
 public class MailServiceImpl implements MailService{
+
+    private final JavaMailSender mailSender;
+
+    @Override
     public MailAuthenticateResponse mailSend(String email) {
-        return null;
+        String randomCode = generateRandomCode();
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Email Verification Code");
+        message.setText("Your verification code is: " + randomCode);
+        mailSender.send(message);
+
+        return new MailAuthenticateResponse(randomCode);
+    }
+    @Override
+    public String generateRandomCode() {
+        Random random = new Random();
+        int code = 100000 + random.nextInt(900000);
+        return String.valueOf(code);
     }
 }
