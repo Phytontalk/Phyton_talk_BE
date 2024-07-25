44402cb70bc74d06cd7db7fe47dc93b32430cb15
Hanjjong <tim4974@naver.com>
2024-07-25 20:32:22 +0900

    [feat] : feat MailServiceImpl

---
+++ src/main/java/like_lion/phytontalk/email/MailServiceImpl.java
@@ -0,0 +1,11 @@
+package like_lion.phytontalk.email;
+
+import like_lion.phytontalk.email.dto.MailAuthenticateResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceImpl implements MailService{
+    public MailAuthenticateResponse mailSend(String email) {
+        return null;
+    }
+}
