5dff45226fae511d2c995817ade9fb7e8ed64bee
mo0n_white <ohyerin02@naver.com>
2024-07-27 04:24:22 +0900

    [feat] : Add Dto structure

--- src/main/java/like_lion/phytontalk/quiz/dto/QuizResponse.java
+++ src/main/java/like_lion/phytontalk/quiz/dto/QuizResponse.java
@@ -1,4 +1,19 @@
 package like_lion.phytontalk.quiz.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuizResponse {
+    private long quizId;
+    private String option1;
+    private String option2;
+    private String type;
+    private int frequency;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 }
