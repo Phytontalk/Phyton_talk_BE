ad7ceb8cb68b60f27c06ed3d632f8ce4daf5476c
mo0n_white <ohyerin02@naver.com>
2024-07-27 04:31:18 +0900

    [comment] : regarding pick 10 quiz

--- src/main/java/like_lion/phytontalk/quiz/QuizRepository.java
+++ src/main/java/like_lion/phytontalk/quiz/QuizRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    // 랜덤 기준? 설정 필요
     @Query(value = "SELECT * FROM quiz ORDER BY RAND() LIMIT 10", nativeQuery = true)
     List<Quiz> findRandomQuiz();
 }
