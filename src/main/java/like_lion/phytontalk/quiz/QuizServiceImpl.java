ee73557a8ce18f1cb0a6e1b88dbd9e96540b46c1
mo0n_white <ohyerin02@naver.com>
2024-07-29 01:28:17 +0900

    [refactor] : Use pageable  / Use orElseThrow

--- src/main/java/like_lion/phytontalk/quiz/QuizServiceImpl.java
+++ src/main/java/like_lion/phytontalk/quiz/QuizServiceImpl.java
@@ -4,6 +4,8 @@ import like_lion.phytontalk.quiz.dto.QuizRequest;
 import like_lion.phytontalk.quiz.dto.QuizResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +14,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class QuizServiceImpl implements QuizService {
     private final QuizRepository quizRepo;
 
     @Override
+    @Transactional
     public QuizResponse createQuiz(QuizRequest quizRequest) { // 퀴즈 생성
         Quiz quiz = new Quiz();
 
@@ -26,50 +29,41 @@ public class QuizServiceImpl implements QuizService {
         quiz.setType(quizRequest.getType());
 
         Quiz savedQuiz = quizRepo.save(quiz);   // request로 받은 객체 저장
-        return mapToResponseDto(quiz);
+        return mapToResponseDto(savedQuiz);
     }
 
     @Override
+    @Transactional
     public QuizResponse updateQuiz(Long id, QuizRequest quizRequest) { // 퀴즈 수정
         Optional<Quiz> quiz = quizRepo.findById(id);
-        if (quiz.isPresent()) {
-            Quiz quizToUpdate = quiz.get();
+        quiz.orElseThrow(()-> new RuntimeException("Quiz not found"));
+        Quiz findQuiz = quiz.get();
 
-            quizToUpdate.setOption1(quizRequest.getOption1());
-            quizToUpdate.setOption2(quizRequest.getOption2());
-            quizToUpdate.setType(quizRequest.getType());
+        findQuiz.setOption1(quizRequest.getOption1());
+        findQuiz.setOption2(quizRequest.getOption2());
+        findQuiz.setType(quizRequest.getType());
 
-            Quiz updatedQuiz = quizRepo.save(quizToUpdate);
-            return mapToResponseDto(updatedQuiz);
-        } else {
-            // 예외처리
-            return null;
-        }
+        return mapToResponseDto(findQuiz);
     }
 
     @Override
+    @Transactional
     public void deleteQuiz(Long id) { // 퀴즈 삭제
-        if (quizRepo.existsById(id)) {  // 직접적 삭제 안할거면 findById로
-            quizRepo.deleteById(id);
-        } else {
-            // 예외처리
-//                throw new ResourceNotFoundException("Quiz not found with id " + id);
-            return;
-        }
+        Optional<Quiz> quiz = quizRepo.findById(id);
+        quiz.orElseThrow(()-> new RuntimeException("Quiz not found"));
+        quizRepo.deleteById(id);
     }
-
 
     @Override
     public List<QuizResponse> getAllQuiz() { // 퀴즈 전체 조회
         return quizRepo.findAll().stream().map(this::mapToResponseDto).collect(Collectors.toList());
     }
 
-
     @Override
-    public List<QuizResponse> getRandomQuiz() { // 퀴즈 오늘의 문제
-        return quizRepo.findRandomQuiz().stream().map(this::mapToResponseDto).collect(Collectors.toList());
+    public List<QuizResponse> getDailyQuiz() { // 퀴즈 오늘의 문제
+        Pageable pageable = PageRequest.of(0, 10);
+        return quizRepo.findDailyQuiz(pageable).stream().map(this::mapToResponseDto).collect(Collectors.toList());
     }
-
 
     private QuizResponse mapToResponseDto(Quiz quiz) {
         QuizResponse quizResponse = new QuizResponse();
