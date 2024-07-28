1edf61662b21b68011d1bee57af8b0e746b4cd14
mo0n_white <ohyerin02@naver.com>
2024-07-29 01:28:17 +0900

    [refactor] : Use pageable  / Use orElseThrow

--- src/main/java/like_lion/phytontalk/quiz/QuizRepository.java
+++ src/main/java/like_lion/phytontalk/quiz/QuizRepository.java
@@ -1,5 +1,6 @@
 package like_lion.phytontalk.quiz;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
     // 랜덤 기준? 설정 필요
-    @Query(value = "SELECT * FROM quiz ORDER BY RAND() LIMIT 10", nativeQuery = true)
-    List<Quiz> findRandomQuiz();
+    @Query("SELECT q FROM Quiz q ORDER BY q.frequency, q.createdAt DESC")
+    List<Quiz> findDailyQuiz(Pageable pageable);
+//    List<Quiz> findAll(Pageable pageable);
 }
