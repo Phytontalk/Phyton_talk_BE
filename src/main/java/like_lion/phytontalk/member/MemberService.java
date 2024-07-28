e8e04a0135206ad0663eb53f6aa660a3f9cf815f
duzzevzn <duzzevzn@gmail.com>
2024-07-29 02:11:59 +0900

    [feat] : modify MemberService

--- src/main/java/like_lion/phytontalk/member/MemberService.java
+++ src/main/java/like_lion/phytontalk/member/MemberService.java
@@ -1,6 +1,8 @@
 package like_lion.phytontalk.member;
 
 import jakarta.servlet.http.HttpSession;
+import like_lion.phytontalk.member.dto.MemberInfoRequest;
+import like_lion.phytontalk.member.dto.MemberUpdateRequest;
 import like_lion.phytontalk.member.dto.SignInRequest;
 import like_lion.phytontalk.member.dto.SignupRequest;
 import org.springframework.stereotype.Service;
@@ -15,4 +17,10 @@ public interface MemberService {
     Optional<Member> findOneMemberByEmail(String email);
 
     void checkMemberPassword(Member member, String inputPassword);
+
+    Member getMemberInfo(MemberInfoRequest request);
+
+    void updateMemberInfo(Long memberId, MemberUpdateRequest request);
+
+    void deleteMember(MemberInfoRequest request);
 }
