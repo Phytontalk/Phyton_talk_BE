f0218f4c8dd528da8bd5cfac89049685428167d4
duzzevzn <duzzevzn@gmail.com>
2024-07-29 02:12:49 +0900

    [feat] : modify MemberServiceImpl

--- src/main/java/like_lion/phytontalk/member/MemberServiceImpl.java
+++ src/main/java/like_lion/phytontalk/member/MemberServiceImpl.java
@@ -1,6 +1,8 @@
 package like_lion.phytontalk.member;
 
 import jakarta.servlet.http.HttpSession;
+import like_lion.phytontalk.member.dto.MemberInfoRequest;
+import like_lion.phytontalk.member.dto.MemberUpdateRequest;
 import like_lion.phytontalk.member.dto.SignInRequest;
 import like_lion.phytontalk.member.dto.SignupRequest;
 import lombok.RequiredArgsConstructor;
@@ -50,5 +52,32 @@ public class MemberServiceImpl implements MemberService {
         if (!member.checkPassword(inputPassword)) {
             throw new IllegalArgumentException("비밀번호 불일치");
         }
+    }
+
+    @Override
+    @Transactional
+    public Member getMemberInfo(MemberInfoRequest request) {
+        Optional<Member> memberOpt = findOneMemberByEmail(request.email());
+        Member member = memberOpt.orElseThrow(() -> new IllegalArgumentException("가입되어 있지 않은 회원입니다."));
+        checkMemberPassword(member, request.password());
+        return member;
+    }
+
+    @Override
+    @Transactional
+    public void updateMemberInfo(Long memberId, MemberUpdateRequest request) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
+        member.setName(request.name());
+        member.setEmail(request.email());
+        member.setPassword(request.password());
+        memberRepository.save(member);
+    }
+
+    @Override
+    @Transactional
+    public void deleteMember(MemberInfoRequest request) {
+        Member member = getMemberInfo(request);
+        memberRepository.delete(member);
     }
 }
