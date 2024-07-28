694117ae2f4d625b3e0a5e3a714c56d5203ba83a
mo0n_white <ohyerin02@naver.com>
2024-07-29 01:25:58 +0900

    [feat] : Add quiz controller

---
+++ src/main/java/like_lion/phytontalk/quiz/QuizController.java
@@ -0,0 +1,48 @@
+package like_lion.phytontalk.quiz;
+
+import like_lion.phytontalk.quiz.dto.QuizRequest;
+import like_lion.phytontalk.quiz.dto.QuizResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/quiz")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class QuizController {
+    private final QuizService quizService;
+
+    @PostMapping
+    public ResponseEntity<QuizResponse> createQuiz(QuizRequest quizRequest) { // 퀴즈 등록
+        QuizResponse createQuiz = quizService.createQuiz(quizRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createQuiz);
+    }
+
+    @PatchMapping("/{quizId}")
+    public ResponseEntity<QuizResponse> updateQuiz(@PathVariable Long quizId, QuizRequest quizRequest) { // 퀴즈 수정
+        QuizResponse updateQuiz = quizService.updateQuiz(quizId, quizRequest);
+        return ResponseEntity.ok(updateQuiz);
+    }
+
+    @DeleteMapping("/{quizId}")
+    public ResponseEntity<Void> deleteQuiz(@PathVariable Long quizId) { // 퀴즈 삭제
+        quizService.deleteQuiz(quizId);
+        return ResponseEntity.noContent().build();  // 응답 본문 필요없는 경우 빈 ResponseEntity 객체 생성
+    }
+
+    @GetMapping
+    public ResponseEntity<List<QuizResponse>> getAllQuiz() { // 퀴즈 전체 조회
+        List<QuizResponse> quiz = quizService.getAllQuiz();
+        return ResponseEntity.ok(quiz);
+    }
+
+    @GetMapping("/daily")
+    public ResponseEntity<List<QuizResponse>> getDailyQuiz() { // 오늘의 퀴즈 조회
+        List<QuizResponse> quiz = quizService.getDailyQuiz();
+        return ResponseEntity.ok(quiz);
+    }
+}
