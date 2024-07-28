d4a58c93e476a7a60f8b4d0fbf4963996b0bccb4
duzzevzn <duzzevzn@gmail.com>
2024-07-29 02:08:53 +0900

    [feat] : add DTOs for Member

--- src/main/java/like_lion/phytontalk/member/dto/MemberInfoRequest.java
+++ src/main/java/like_lion/phytontalk/member/dto/MemberInfoRequest.java
@@ -1,2 +1,4 @@
-package like_lion.phytontalk.member.dto;public record MemberInfoRequest() {
+package like_lion.phytontalk.member.dto;
+
+public record MemberInfoRequest(String email, String password) {
 }
--- src/main/java/like_lion/phytontalk/member/dto/MemberUpdateRequest.java
+++ src/main/java/like_lion/phytontalk/member/dto/MemberUpdateRequest.java
@@ -1,2 +1,4 @@
-package like_lion.phytontalk.member.dto;public record MemberUpdateRequest() {
+package like_lion.phytontalk.member.dto;
+
+public record MemberUpdateRequest(String name, String email, String password) {
 }
