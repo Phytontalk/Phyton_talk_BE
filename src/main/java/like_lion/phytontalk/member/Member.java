a3d8eee19dd03af9bf00ce8e47188015f73a963c
duzzevzn <duzzevzn@gmail.com>
2024-07-30 01:47:42 +0900

    [feat] : modify Member Entity

--- src/main/java/like_lion/phytontalk/member/Member.java
+++ src/main/java/like_lion/phytontalk/member/Member.java
@@ -1,18 +1,18 @@
 package like_lion.phytontalk.member;
 
 import jakarta.persistence.*;
-import like_lion.phytontalk.answer.Answer;
 import like_lion.phytontalk.avatar.Avatar;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -38,29 +38,7 @@ public class Member {
     @JoinColumn(name = "avatar_id", referencedColumnName = "avatarId")
     private Avatar avatar;
 
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Answer> answer;
-    public boolean checkPassword(String password){
+    public boolean checkPassword(String password) {
         return Objects.equals(this.password, password);
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setSns(String sns) {
-        this.sns = sns;
-    }
-
-    public void setAvatar(Avatar avatar) {
-        this.avatar = avatar;
     }
 }
