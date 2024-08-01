8ec5af5187d2b2ca8dfd8d1cbc049e8d269975a0
Hanjjong <tim4974@naver.com>
2024-08-02 01:54:04 +0900

    [feat] : add answer list to memberEntity

--- src/main/java/like_lion/phytontalk/member/Member.java
+++ src/main/java/like_lion/phytontalk/member/Member.java
@@ -1,12 +1,15 @@
 package like_lion.phytontalk.member;
 
 import jakarta.persistence.*;
+import like_lion.phytontalk.answer.Answer;
 import like_lion.phytontalk.avatar.Avatar;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -36,6 +39,9 @@ public class Member {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "avatar_id", referencedColumnName = "avatarId")
     private Avatar avatar;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Answer> answers;
 
     public boolean checkPassword(String password) {
         return Objects.equals(this.password, password);
