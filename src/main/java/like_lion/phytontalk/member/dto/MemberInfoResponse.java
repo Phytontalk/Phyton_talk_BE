c8d792f5c6939bd781d111561834102ece7bf333
duzzevzn <duzzevzn@gmail.com>
2024-08-02 23:57:15 +0900

    [feat] : modify DTOs for Member

--- src/main/java/like_lion/phytontalk/member/dto/MemberInfoResponse.java
+++ src/main/java/like_lion/phytontalk/member/dto/MemberInfoResponse.java
@@ -1,4 +1,4 @@
 package like_lion.phytontalk.member.dto;
 
-public record MemberInfoResponse(String name, String email, String sns, Long avatarId) {
+public record MemberInfoResponse(String name, String email, String sns, Long avatarId, int birthYear) {
 }
